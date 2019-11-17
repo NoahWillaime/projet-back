6d36aa4c583aef73a04672b7dcb006550c62aa45
Noah <noah.willaime@gmail.com>
2019-11-17 16:34:43 +0100

    Upgrade du script init mongodb

--- scripts/init.mongodb.js
+++ scripts/init.mongodb.js
@@ -1,39 +1,3 @@
-db.getCollection('animals').insertMany([
-      {
-        "name": "Myke",
-        "photo": "https://randomuser.me/portraits/women/59.jpg",
-        "species": "Dog",
-        "breed": "idk",
-        "gender": "Male",
-        "diet": "Meat and letuce",
-        "health": "Good Health",
-        "description": "a bit loud",
-        "enterDate": ISODate("2018-01-01T23:00:00.000Z"),
-      },
-      {
-        "name": "Catty",
-        "photo": "https://randomuser.me/portraits/women/59.jpg",
-        "species": "Cat",
-        "breed": "Persian",
-        "gender": "Male",
-        "diet": "Mise",
-        "health": "Not so good Health",
-        "description": "Very calm",
-        "enterDate": ISODate("2008-01-01T23:00:00.000Z"),
-      },
-      {
-        "name": "jack",
-        "photo": "https://randomuser.me/portraits/women/59.jpg",
-        "species": "Sparrow",
-        "breed": "pirate",
-        "gender": "Female",
-        "diet": "salted tears",
-        "health": "Probably dead now",
-        "description": "IDK",
-        "enterDate": ISODate("1974-01-01T23:00:00.000Z"),
-      }
-]);
-
 db.getCollection('refuges').insertMany([
   {
     "name": "Refuge de Nancy",
@@ -42,6 +6,7 @@ db.getCollection('refuges').insertMany([
       "postalCode": NumberInt(54000),
       "city": "Nancy"
     },
+    "animalsIds": [],
     "phone": "+33145653290",
     "email": "refuge.nancy@fictive.com"
   },
@@ -52,9 +17,61 @@ db.getCollection('refuges').insertMany([
       "postalCode": NumberInt(54400),
       "city": "Longwy"
     },
+    "animalsIds": [],
     "phone": "+33123453290",
     "email": "refuge.longwy@fictive.com"
   }
 ]);
 
-db.getCollection('people').find({});
+db.getCollection('animals').insertMany([
+  {
+    "name": "Myke",
+    "photo": "https://randomuser.me/portraits/women/59.jpg",
+    "species": "Dog",
+    "breed": "idk",
+    "gender": "Male",
+    "diet": "Meat and letuce",
+    "health": "Good Health",
+    "description": "a bit loud",
+    "enterDate": ISODate("2018-01-01T23:00:00.000Z"),
+    "refugeName": "Refuge de Nancy"
+  },
+  {
+    "name": "Catty",
+    "photo": "https://randomuser.me/portraits/women/59.jpg",
+    "species": "Cat",
+    "breed": "Persian",
+    "gender": "Male",
+    "diet": "Mise",
+    "health": "Not so good Health",
+    "description": "Very calm",
+    "enterDate": ISODate("2008-01-01T23:00:00.000Z"),
+    "refugeName": "Refuge de Nancy"
+  },
+  {
+    "name": "jack",
+    "photo": "https://randomuser.me/portraits/women/59.jpg",
+    "species": "Sparrow",
+    "breed": "pirate",
+    "gender": "Female",
+    "diet": "salted tears",
+    "health": "Probably dead now",
+    "description": "IDK",
+    "enterDate": ISODate("1974-01-01T23:00:00.000Z"),
+    "refugeName": "Refuge de Longwy"
+  }
+]);
+
+var data = db.getCollection('animals').find({}).map(function(element) {
+  return { _id: element._id, refugeName: element.refugeName, name: element.name };
+});
+data.forEach(function(element) {
+  var refuges = db.getCollection('refuges').find({"name": element.refugeName}).map(function(elt) {
+    return { _id: elt._id, name: elt.name, animalsIds: elt.animalsIds };
+  });
+  if (!!refuges) {
+    db.getCollection('animals').updateOne({_id: element._id}, { $set: { refugeId: refuges[0]._id } });
+    var Ids = refuges[0].animalsIds.concat(element._id);
+    db.getCollection('refuges').updateOne({_id: refuges[0]._id}, { $set: { animalsIds: Ids } });
+  }
+});
