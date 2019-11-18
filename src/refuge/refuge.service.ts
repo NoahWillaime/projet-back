1521457dc573d1e71da674079e7d9a9e04963c79
spatzouil <clement.bacq@free.fr>
2019-11-18 15:56:57 +0100

    ajout findAllByPostalCode (refuge)

--- src/refuge/refuge.service.ts
+++ src/refuge/refuge.service.ts
@@ -3,6 +3,7 @@ import { Observable, of, throwError } from 'rxjs';
 import { catchError, flatMap, map } from 'rxjs/operators';
 import { RefugesDao } from './dao/refuges.dao';
 import { RefugeEntity } from './entities/refuge.entity';
+import { AnimalEntity } from '../animals/entities/animal.entity';
 
 @Injectable()
 export class RefugeService {
@@ -12,6 +13,19 @@ export class RefugeService {
     return this._refugeDao.find()
       .pipe(
         map(_ => (!!_) ? _.map(__ => new RefugeEntity(__)) : undefined),
+      );
+  }
+
+  findAllByPostalCode(postalcode: string): Observable<RefugeEntity[] | void> {
+    return this.findAll()
+      .pipe(
+        map((refuges: RefugeEntity[]) =>
+          refuges.filter((refuges: RefugeEntity) => refuges.address.postalCode.toLowerCase() === postalcode.toLowerCase())),
+        flatMap(_ =>
+          (!!_ && _.length > 0) ?
+            of(_) :
+            throwError(new NotFoundException('No animal with specified species here'))
+        ),
       );
   }
 
