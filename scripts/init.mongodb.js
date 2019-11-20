89f98079974346563740365bf1e348592f80e74f
spatzouil <clement.bacq@free.fr>
2019-11-20 02:01:38 +0100

    pull2

--- scripts/init.mongodb.js
+++ scripts/init.mongodb.js
@@ -42,40 +42,46 @@ db.getCollection('benevoles').insertMany([
 
 db.getCollection('animals').insertMany([
   {
-    "name": "Myke",
-    "photo": "https://randomuser.me/portraits/women/59.jpg",
-    "species": "Dog",
-    "breed": "idk",
-    "gender": "Male",
-    "diet": "Meat and letuce",
-    "health": "Good Health",
-    "description": "a bit loud",
-    "enterDate": ISODate("2018-01-01T23:00:00.000Z"),
-    "refugeName": "Refuge de Nancy"
+    "_id" : ObjectId("5dd47597d2c07771668100d5"),
+    "name" : "Myke",
+    "photo" : "https://www.pets4homes.co.uk/images/classifieds/2018/12/05/2149499/large/beautiful-kc-registered-golden-lab-for-sale-5c082ec2b5b7b.jpg",
+    "species" : "Dog",
+    "breed" : "Labrador",
+    "gender" : "Male",
+    "diet" : "Meat and letuce",
+    "health" : "Good Health",
+    "description" : "a bit loud",
+    "enterDate" : ISODate("2018-01-01T23:00:00.000Z"),
+    "refugeName" : "Refuge de Nancy",
+    "refugeId" : ObjectId("5dd47597d2c07771668100d1")
   },
   {
-    "name": "Catty",
-    "photo": "https://randomuser.me/portraits/women/59.jpg",
-    "species": "Cat",
-    "breed": "Persian",
-    "gender": "Male",
-    "diet": "Mise",
-    "health": "Not so good Health",
-    "description": "Very calm",
-    "enterDate": ISODate("2008-01-01T23:00:00.000Z"),
-    "refugeName": "Refuge de Nancy"
+    "_id" : ObjectId("5dd47597d2c07771668100d6"),
+    "name" : "Catty",
+    "photo" : "https://www.thisiscolossal.com/wp-content/uploads/2019/08/cat-hat-8.jpg",
+    "species" : "Cat",
+    "breed" : "Persian",
+    "gender" : "Male",
+    "diet" : "Mise",
+    "health" : "Not so good Health",
+    "description" : "Very calm",
+    "enterDate" : ISODate("2008-01-01T23:00:00.000Z"),
+    "refugeName" : "Refuge de Nancy",
+    "refugeId" : ObjectId("5dd47597d2c07771668100d1")
   },
   {
-    "name": "jack",
-    "photo": "https://randomuser.me/portraits/women/59.jpg",
-    "species": "Sparrow",
-    "breed": "pirate",
-    "gender": "Female",
-    "diet": "salted tears",
-    "health": "Probably dead now",
-    "description": "IDK",
-    "enterDate": ISODate("1974-01-01T23:00:00.000Z"),
-    "refugeName": "Refuge de Longwy"
+    "_id" : ObjectId("5dd47597d2c07771668100d7"),
+    "name" : "jack",
+    "photo" : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQeux5ZErmaNVpISV1_Uz0b_-OMMADLRC9q-Sg8SWu6OTDynZ_u",
+    "species" : "Bird",
+    "breed" : "Ara",
+    "gender" : "Female",
+    "diet" : "Seed",
+    "health" : "Too much",
+    "description" : "IDK",
+    "enterDate" : ISODate("1974-01-01T23:00:00.000Z"),
+    "refugeName" : "Refuge de Longwy",
+    "refugeId" : ObjectId("5dd47597d2c07771668100d2")
   }
 ]);
 
